8ed768443a3fd17c1914b7a5d21a7f82d020c61a
PeterShin23 <pss9fp@virginia.edu>
2024-04-03 18:26:59 -0400

    adding front page

---
+++ my-site/src/utils/colors.ts
@@ -0,0 +1,11 @@
+export enum Colors {
+  EnglishViolet = '#413C58',
+  AshGray = '#A3C4BC',
+  AshGrayShadow = '#283E39',
+  TeaGreenDark = '#abc1a2',
+  TeaGreen = '#BFD7B5',
+  TeaGreenLight = '#c5dbbc',
+  Cream = '#E7EFC5',
+  Parchment ='#F2E8C9',
+  Wenge = '#56494C'
+};
