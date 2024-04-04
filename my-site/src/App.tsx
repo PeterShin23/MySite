83dc6e1c6b3061c854424942d309f69751169004
PeterShin23 <pss9fp@virginia.edu>
2024-04-03 20:19:03 -0400

    adding headline and scrolling page

--- my-site/src/App.tsx
+++ my-site/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { HeroContainer } from './components/hero';
+import { TimelineContainer } from './components/timeline';
 import logo from './logo.svg';
 // import './App.css';
 
 const App = () => {
+
   return (
-    <div>
-      <HeroContainer />
+    <div className="flex flex-col h-screen overflow-hidden">
+      <header className="w-full h-16 text-center border-b border-grey p-4">
+        <div className="flex flex-row justify-end items-center mr-8">
+          <button>Top</button>
+          <button>Timeline</button>
+        </div>
+      </header>
+      <main className="flex-1 overflow-y-auto">
+        <HeroContainer />
+        <TimelineContainer />
+      </main>
     </div>
   );
 }
