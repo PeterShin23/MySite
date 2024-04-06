b3eb6e304087904d188fb1f6b09d7dfa6b95a08c
PeterShin23 <pss9fp@virginia.edu>
2024-04-06 19:32:53 -0400

    adding skills cloud

---
+++ my-site/src/components/skills/skills-cloud.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import {Cloud, renderSimpleIcon, SimpleIcon} from "react-icon-cloud"
+import { skills } from "./skills";
+
+export const SkillsCloud = () => {
+
+  const onSkillClick = (e, infoLink) => {
+    e.preventDefault();
+
+    setTimeout(() => window.open(infoLink, "_blank", "noreferrer,noopener"), 500);
+  }
+
+  return (
+    <Cloud 
+      // https://www.goat1000.com/tagcanvas-options.php
+      options={{
+        outlineColour: "#0000",
+        clickToFront: 500,
+        wheelZoom: false,
+        initial: [0.05, -0.05],
+        dragControl: true,
+        minSpeed: 0.01
+      }}>
+      {
+        skills.map((skill) => (
+          <a 
+            key={skill.name} 
+            onClick={(e) => onSkillClick(e, skill.info)}
+          >
+            <img className="focus-ring:none" src={skill.svg} />
+          </a>
+        ))
+      }
+    </Cloud>
+  )
+}
+
+/** Memoized skills component to prevent unnecessary rerender of skills cloud and awkward movement stopping */
+export const MemoizedSkillsCloud = React.memo(SkillsCloud);
---
+++ my-site/src/components/skills/skills.js
@@ -0,0 +1,160 @@
+import icon_awsApiGateway from "../../static/aws-api-gateway.svg";
+import icon_awsLambda from "../../static/aws-lambda.svg";
+import icon_awsS3 from "../../static/aws-s3.svg";
+import icon_cPlusPlus from "../../static/c-plusplus.svg";
+import icon_cSharp from "../../static/c-sharp.svg";
+import icon_css3 from "../../static/css-3.svg";
+import icon_django from "../../static/django.svg"
+import icon_docker from "../../static/docker-icon.svg"
+import icon_dotnet from "../../static/dotnet.svg"
+import icon_elasticsearch from "../../static/elasticsearch.svg"
+import icon_firebase from "../../static/firebase.svg"
+import icon_git from "../../static/git-icon.svg"
+import icon_github from "../../static/github-icon.svg"
+import icon_html from "../../static/html-5.svg"
+import icon_java from "../../static/java.svg"
+import icon_javascript from "../../static/javascript.svg"
+import icon_kotlin from "../../static/kotlin-icon.svg"
+import icon_kubernetes from "../../static/kubernetes.svg"
+import icon_microsoftAzure from "../../static/microsoft-azure.svg"
+import icon_mySQL from "../../static/mysql-icon.svg"
+import icon_nodeJS from "../../static/nodejs-icon.svg"
+import icon_postgreSQL from "../../static/postgresql.svg"
+import icon_python from "../../static/python.svg"
+import icon_react from "../../static/react.svg"
+import icon_tailwindcss from "../../static/tailwindcss-icon.svg"
+import icon_typescript from "../../static/typescript-icon.svg"
+
+/** Icons taken from https://svgporn.com/ */
+export const skills = [
+  {
+    "name": "AWS API Gateway",
+    "svg": icon_awsApiGateway,
+    "info": "https://aws.amazon.com/api-gateway/"
+  },
+  {
+    "name": "AWS Lambda",
+    "svg": icon_awsLambda,
+    "info": "https://aws.amazon.com/lambda/"
+  },
+  {
+    "name": "AWS S3",
+    "svg": icon_awsS3,
+    "info": "https://aws.amazon.com/s3/"
+  },
+  {
+    "name": "C++",
+    "svg": icon_cPlusPlus,
+    "info": "https://cplusplus.com/"
+  },
+  {
+    "name": "C#",
+    "svg": icon_cSharp,
+    "info": "https://learn.microsoft.com/en-us/dotnet/csharp/"
+  },
+  {
+    "name": "CSS",
+    "svg": icon_css3,
+    "info": "https://developer.mozilla.org/en-US/docs/Web/CSS"
+  },
+  {
+    "name": "Django",
+    "svg": icon_django,
+    "info": "https://docs.djangoproject.com/en"
+  },
+  {
+    "name": "Docker",
+    "svg": icon_docker,
+    "info": "https://www.docker.com/"
+  },
+  {
+    "name": ".NET",
+    "svg": icon_dotnet,
+    "info": "https://dotnet.microsoft.com/en-us/learn/dotnet/what-is-dotnet"
+  },
+  {
+    "name": "Elasticsearch",
+    "svg": icon_elasticsearch,
+    "info": "https://www.elastic.co/guide/en/elasticsearch/reference/master/index.html"
+  },
+  {
+    "name": "Firebase",
+    "svg": icon_firebase,
+    "info": "https://firebase.google.com/"
+  },
+  {
+    "name": "Git",
+    "svg": icon_git,
+    "info": "https://git-scm.com/book/en/v2/Getting-Started-What-is-Git%3F"
+  },
+  {
+    "name": "Github",
+    "svg": icon_github,
+    "info": "https://github.com/"
+  },
+  {
+    "name": "HTML",
+    "svg": icon_html,
+    "info": "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"
+  },
+  {
+    "name": "Java",
+    "svg": icon_java,
+    "info": "https://www.java.com/en/"
+  },
+  {
+    "name": "JavaScript",
+    "svg": icon_javascript,
+    "info": "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+  },
+  {
+    "name": "Kotlin",
+    "svg": icon_kotlin,
+    "info": "https://kotlinlang.org/"
+  },
+  {
+    "name": "Kubernetes",
+    "svg": icon_kubernetes,
+    "info": "https://kubernetes.io/"
+  },
+  {
+    "name": "Microsoft Azure",
+    "svg": icon_microsoftAzure,
+    "info": "https://azure.microsoft.com/en-us"
+  },
+  {
+    "name": "MySQL",
+    "svg": icon_mySQL,
+    "info": "https://www.mysql.com/"
+  },
+  {
+    "name": "Node.JS",
+    "svg": icon_nodeJS,
+    "info": "https://nodejs.org/en"
+  },
+  {
+    "name": "PostgreSQL",
+    "svg": icon_postgreSQL,
+    "info": "https://www.postgresql.org/"
+  },
+  {
+    "name": "Python",
+    "svg": icon_python,
+    "info": "https://www.python.org/"
+  },
+  {
+    "name": "React",
+    "svg": icon_react,
+    "info": "https://react.dev/"
+  },
+  {
+    "name": "Tailwind CSS",
+    "svg": icon_tailwindcss,
+    "info": "https://tailwindcss.com/"
+  },
+  {
+    "name": "TypeScript",
+    "svg": icon_typescript,
+    "info": "https://www.typescriptlang.org/"
+  }
+]
