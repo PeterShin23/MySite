76979297a21bca41d5a3a776ca7a92aca921738e
PeterShin23 <pss9fp@virginia.edu>
2024-04-07 02:13:51 -0400

    adding more timeline styling and card

--- my-site/src/components/timeline/my-events.js
+++ my-site/src/components/timeline/my-events.js
@@ -1,43 +1,54 @@
+import uvaCavaliers from "../../static/virginia-cavaliers.svg";
+import uvaBci from "../../static/uva-bci.svg";
+import awsBrand from "../../static/aws-brand.svg";
+import costarBrand from "../../static/CSGP.svg";
+
 export const events = {
   0: {
     startDate: "08-01-2019",
     endDate: "05-01-2023",
     title: "University of Virginia",
     subtitle: "School of Engineering and Applied Science",
-    description: "event 1"
+    description: "event 1",
+    image: uvaCavaliers,
   },
   1: {
     startDate: "08-01-2020",
     endDate: "05-01-2022",
     title: "Teaching Assistant",
     subtitle: "Software Development Methods in Java",
-    description: "event 2"
+    description: "event 2",
+    image: uvaCavaliers,
   },
   2: {
     startDate: "08-01-2021",
     endDate: "05-01-2023",
     title: "University of Virginia",
     subtitle: "School of Data Science",
-    description: "event 2"
+    description: "event 2",
+    image: uvaCavaliers,
   },
   3: {
     startDate: "02-01-2022",
     endDate: "05-01-2022",
     title: "Undergraduate Research Assistant",
     subtitle: "UVA Biocomplexity Institute",
-    description: "event 3"
+    description: "event 3",
+    image: uvaBci,
   },
   4: {
     startDate: "05-01-2022",
     endDate: "08-01-2022",
     title: "Software Development Engineer Intern",
     subtitle: "Amazon Web Services",
-    description: "event 4"
+    description: "event 4",
+    image: awsBrand,
   },
   5: {
     startDate: "07-01-2023",
     title: "CoStar Group",
     subtitle: "Associate Software Engineer",
-    description: "event 5"
+    description: "event 5",
+    image: costarBrand,
   },
 };
